cac5e1cbe1b901dd0d83034ea48c6c8bef8f5c12
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-27 13:57:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { Client } from "discord.js";
 
 import { TOKEN } from './utils/config.js';
-import eventLoader from './utils/loaders/eventLoader';
+import eventLoader from './utils/loaders/eventLoader.js';
+import commandLoader from './utils/loaders/commandLoader.js'
 
 const CLIENT = new Client({ intents: 1 });
 
 eventLoader(CLIENT);
+commandLoader(CLIENT);
 
 CLIENT.login(TOKEN);
