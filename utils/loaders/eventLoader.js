7509f398454c39a944fbe64e25d2d8d36ddcb6d0
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-27 12:12:47 +0100

    Update eventLoader.js

--- utils/loaders/eventLoader.js
+++ utils/loaders/eventLoader.js
@@ -1,1 +1,10 @@
+import handle from "./handle.js";
 
+export default function(client) {
+    handle('events', async path => {
+        const eventName = path.split('/').pop().split('.')[0],
+            eventCode = await import(path);
+        client.on(eventName, eventCode.default);
+        console.log(`Event ${eventName} chargé`)
+    });
+};
