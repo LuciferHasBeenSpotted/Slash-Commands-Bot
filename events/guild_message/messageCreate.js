1d2f3085152e7abcb70df3d26ebf44e40e9b6bb3
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-27 12:25:48 +0100

    Update messageCreate.js

--- events/guild_message/messageCreate.js
+++ events/guild_message/messageCreate.js
@@ -1,1 +1,11 @@
+import { PREFIX } from "../../utils/config.js";
 
+export default async function(message) {
+    if(!message.content.endsWith(PREFIX)) return;
+    const args = message.content.split(PREFIX)[0].trim().split(' ');
+    const commandName = args.shift().toLowerCase();
+    const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases.includes(commandName));
+
+    if(!command) return;
+    command.run(message, args);
+};
