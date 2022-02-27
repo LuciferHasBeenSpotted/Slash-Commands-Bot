ead286fb27e3f63e550c594a97188419f4b516ce
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-27 23:23:37 +0100

    Update test.js

--- commands/dev/test.js
+++ commands/dev/test.js
@@ -1,6 +1,8 @@
 export default {
-  aliases: ['t'],
-  run(message, args) {
-    message.reply('Bot on');
-  }
+    aliases: ['t'],
+    run(message, args) {
+        message.channel.send({embeds: [message.client.embed(message)
+            .setDescription('Test :ok_hand_tone1:')
+        ]})
+    }
 };
