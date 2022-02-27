d7a9488548623ba37305028e4ad517db5b834d62
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-27 23:19:32 +0100

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -1,15 +1,17 @@
 import { Client, Collection, MessageEmbed } from "discord.js"
 
 const CLIENT = new Client({ intents: 513 });
+const PP = 'https://images-ext-1.discordapp.net/external/_muelNMDX6BABFmU9QBaf4RUefbRFyy-IEMp0BCZr7M/https/cdn.discordapp.com/avatars/412723052476891136/3fda9d70e3656d2eaf18e3c54890f58d.webp';
 
 CLIENT.commands = new Collection();
 CLIENT.slashCommands = [];
-CLIENT.embed = message => new MessageEmbed() 
-    .setTitle("Commande effectuée avec succès")
-    .setAuthor({text: "Developped by 『🌸』 嵐 Lucifer リーダー『🍁』#1293", iconURL: 'https://images-ext-1.discordapp.net/external/U-ACd5WTYVDITIkNSaord4nSHVWurCsGnc5rgGPoaTI/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/412723052476891136/3fda9d70e3656d2eaf18e3c54890f58d.webp?width=647&height=647'})
-    .setURL('www.github.com/luciferHasBeenSpotted')
-    .setColor('DARK_PURPLE')
+CLIENT.embed = function(message, type) { 
+    return new MessageEmbed() 
+    .setAuthor({ name: `Demandée par ${message.author.username}` })
+    .setTitle(type === 'err' ? 'Erreur...' : 'Commande effectuée avec succès !')
+    .setColor(type === 'err' ? 'RED' : 'LIGHT_GREY')
     .setThumbnail(message.author.displayAvatarURL())
     .setTimestamp()
-
+    .setFooter({ text: "Developped by 『🌸』 嵐 Lucifer リーダー『🍁』#1293", iconURL: PP })
+}
 export default CLIENT
