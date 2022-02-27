7feea3cc5032054b9ef899ba9e13da9c545c18d4
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-27 23:22:58 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,1 +1,6 @@
-export default client => console.log("Je suis log bg");
+export default function(client) {
+    console.log('Je suis log bg');
+
+    const devGuild = client.guilds.cache.get('854716033231224842');
+    devGuild.commands.set(client.slashCommands);
+}
