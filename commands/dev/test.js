e2d188ea97436b52bccd50504d2c1696c636d859
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2022-02-28 09:06:49 +0100

    Delete commands directory

--- commands/dev/test.js
+++
@@ -1,8 +0,0 @@
-export default {
-    aliases: ['t'],
-    run(message, args) {
-        message.channel.send({embeds: [message.client.embed(message)
-            .setDescription('Test :ok_hand_tone1:')
-        ]})
-    }
-};
